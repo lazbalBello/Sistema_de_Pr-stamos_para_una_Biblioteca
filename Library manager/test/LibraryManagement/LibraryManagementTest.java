97baadb9f2bdc238e3273986af9c3a82f17327ff
Agust999 <agustincanizares8@gmail.com>
2024-08-26 11:38:57 -0400

    test searchBook ok

--- Library manager/test/LibraryManagement/LibraryManagementTest.java
+++ Library manager/test/LibraryManagement/LibraryManagementTest.java
@@ -30,46 +30,7 @@ public class LibraryManagementTest {
     author = new Author("32141", "StephenKing");
     book = new Book("3242423", "It", author, "Horror", 1978);
     }
-    
-    @Test
-    public void registrerBook(){
-        Result result =  library.registrerBook(book);
-        assertTrue(result.isSuccess());
-        assertEquals("Book "+ book.getTitle() + " successfully registered",result.getMessage());
-      }
-    
-    @Test
-    public void registrerBookDuplicate(){
-        library.registrerBook(book);
-        Result result = library.registrerBook(book);
-        assertFalse(result.isSuccess());    
-    }
-    
-    @Test
-    public void loanBook(){
-        library.registrerBook(book);
-        Result result = library.loanBook("040124", "It", "20/08/2024", "25/08/2024");
-        assertTrue(result.isSuccess());
-        assertEquals("Book loaned successfully.", result.getMessage());
-    }
-    
-    @Test
-    public void loanBookNotAvailable(){
-        library.registrerBook(book);
-        library.loanBook("040124", "It", "20/08/2024", "25/08/2024");
-        Result result = library.loanBook("040124", "It", "20/08/2024", "25/08/2024");
-        assertFalse(result.isSuccess());
-        assertEquals("Book is not available. Book is already loaned out", result.getMessage());
-    }
-    
-    @Test
-    public void loanBookNotExist(){
-        Result result = library.loanBook("0101", "calculo", "20/08/2024", "25/08/2024");
-        assertFalse(result.isSuccess());
-        assertEquals("Book does not exist in the library", result.getMessage());
         
-    }
-    
     @Test
     public void SearchBookFound(){
         library.registrerBook(book);
